37e7eaa7d00ff4ef952988a663ca0be94ff2303c
Outof-ctrl <Mister.N0_N4M3@proton.me>
2025-01-27 00:00:12 +0100

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Afficher un message pour confirmer que le script a été exécuté
+echo Le bouton fonctionne ! > %TEMP%\test_button_result.txt
+echo Le fichier batch a été exécuté correctement.
+pause
+
+REM Ouvrir le fichier texte créé pour confirmer visuellement
+start notepad %TEMP%\test_button_result.txt
